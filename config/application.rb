f08f8f23e6f77cd4ca386b80faf06b8650b8e6b1
dmitry <ovcharenkodmitry@gmail.com>
2013-06-27 23:21:13 -0700

    to heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module VkontakteOnRails
     
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # deploy to heroku
+    config.assets.initialize_on_precompile = false
   end
 end
